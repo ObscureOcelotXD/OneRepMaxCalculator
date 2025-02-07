e4aa8b5f006398403956446c8c683c6a6096e846
Mitchell Spurgat <mspurgat93@gmail.com>
2025-02-07 17:39:51 -0600

    Adding Protein consumption info.

--- OneRepMaxCalculator/OneRepMaxCalculator.cpp
+++ OneRepMaxCalculator/OneRepMaxCalculator.cpp
@@ -10,25 +10,32 @@ int main()
     double weight = 0;
     double numberOfReps = 0;
     double oneRepMax = 0.0;
+
     int formula = 0;
+    int calculationChoice = 0;
+
     std::string formulaName = "";
 
-    std::cout << "Enter Weight\n";
-    std::cin >> weight;
+    std::cout << "Enter 1 for one rep max calculator. Enter 2 for Protien consumption Calculator\n";
+    std::cin >> calculationChoice;
 
-    std::cout << "Enter Number of Reps\n";
-    std::cin >> numberOfReps;
+    if (calculationChoice == 1) {
 
-    std::cout << "Select Calculation Method: \n";
-    std::cout << "1. Bryzcki Formula\n";
-    std::cout << "2. Epley Formula\n";
-    std::cout << "3. Lombardi Formula\n";
-    std::cout << "4. O'Conner Formula\n";
+        std::cout << "Enter Weight Lifted\n";
+        std::cin >> weight;
 
-    std::cin >> formula;
+        std::cout << "Enter Number of Reps\n";
+        std::cin >> numberOfReps;
 
+        std::cout << "Select Calculation Method: \n";
+        std::cout << "1. Bryzcki Formula\n";
+        std::cout << "2. Epley Formula\n";
+        std::cout << "3. Lombardi Formula\n";
+        std::cout << "4. O'Conner Formula\n";
 
-    switch (formula) {
+        std::cin >> formula;
+
+        switch (formula) {
         case 1:
             oneRepMax = weight * (36 / (37 - numberOfReps));
             formulaName = "Bryzcki Formula";
@@ -45,12 +52,39 @@ int main()
             oneRepMax = weight * (1 + (0.025 * numberOfReps));
             formulaName = "O'Conner Formula";
             break;
+        }
+
+        std::cout << "According to the " << formulaName << " Your one rep max is : " << oneRepMax << "\n";
     }
 
-    
+    else if (calculationChoice == 2) {
 
+        double weight = 0;
+        double protienGainLow = 0;
+        double protienGainHigh = 0;
+        double protienFatLossLow = 0;
+        double protienFatLossHigh = 0;
+        double protienMaintenenceLow = 0;
+        double protienMaintenenceHigh = 0;
 
-    std::cout << "According to the " << formulaName << " Your one rep max is : " << oneRepMax;
+        std::cout << "Enter Weight in lb \n";
+        std::cin >> weight;
 
+        protienGainLow = weight * (0.7);
+        protienGainHigh = weight * (1.0);
+
+        protienFatLossLow = weight * (0.9);
+        protienFatLossHigh = weight * (1.4);
+
+        protienMaintenenceLow = weight * (0.5);
+        protienMaintenenceHigh = weight * (0.7);
+
+        std::cout << "To gain muscle you should consume between: " << protienGainLow << " and " << protienGainHigh << " grams of protien. \n";
+        std::cout << "To achive fat loss and maintain you should consume between: " << protienFatLossLow << " and " << protienFatLossHigh << " grams of protien. \n";
+        std::cout << "To maintain muscle you should consume between: " << protienMaintenenceLow << " and " << protienMaintenenceHigh << " grams of protien. \n";
+
+    }
+
+    system("pause");
 }
 
