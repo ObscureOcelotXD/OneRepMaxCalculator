68553593fe5edd25f8b38f604c6996e44c76498d
Mitchell Spurgat <mspurgat93@gmail.com>
2025-02-07 18:04:02 -0600

    adding carbs and fats to protien calculator

--- OneRepMaxCalculator/OneRepMaxCalculator.cpp
+++ OneRepMaxCalculator/OneRepMaxCalculator.cpp
@@ -60,12 +60,27 @@ int main()
     else if (calculationChoice == 2) {
 
         double weight = 0;
+
         double protienGainLow = 0;
         double protienGainHigh = 0;
         double protienFatLossLow = 0;
         double protienFatLossHigh = 0;
         double protienMaintenenceLow = 0;
         double protienMaintenenceHigh = 0;
+
+        double carbsGainLow = 0;
+        double carbsGainHigh = 0;
+        double carbsFatLossLow = 0;
+        double carbsFatLossHigh = 0;
+        double carbsMaintenenceLow = 0;
+        double carbsMaintenenceHigh = 0;
+
+        double fatsGainLow = 0;
+        double fatsGainHigh = 0;
+        double fatsFatLossLow = 0;
+        double fatsFatLossHigh = 0;
+        double fatsMaintenenceLow = 0;
+        double fatsMaintenenceHigh = 0;
 
         std::cout << "Enter Weight in lb \n";
         std::cin >> weight;
@@ -79,10 +94,43 @@ int main()
         protienMaintenenceLow = weight * (0.5);
         protienMaintenenceHigh = weight * (0.7);
 
-        std::cout << "To gain muscle you should consume between: " << protienGainLow << " and " << protienGainHigh << " grams of protien. \n";
-        std::cout << "To achive fat loss and maintain you should consume between: " << protienFatLossLow << " and " << protienFatLossHigh << " grams of protien. \n";
-        std::cout << "To maintain muscle you should consume between: " << protienMaintenenceLow << " and " << protienMaintenenceHigh << " grams of protien. \n";
+        //bulking
+        carbsGainLow = weight * (1.8);
+        carbsGainHigh = weight * (2.7);
 
+        //lean out
+        carbsFatLossLow = weight * (0.9);
+        carbsFatLossHigh = weight * (1.8);
+
+        //maintain
+        carbsMaintenenceLow = weight * (1.4);
+        carbsMaintenenceHigh = weight * (2.3);
+
+
+        //bulking
+        fatsGainLow = weight * (0.35);
+        fatsGainHigh = weight * (0.55);
+
+        //lean out
+        fatsFatLossLow = weight * (0.25);
+        fatsFatLossHigh = weight * (0.45);
+
+        //maintain
+        fatsMaintenenceLow = weight * (0.35);
+        fatsMaintenenceHigh = weight * (0.45);
+
+
+        std::cout << "To gain muscle (Bulking) you should consume between: " << protienGainLow << " and " << protienGainHigh << " grams of protien. \n";
+        std::cout << "To gain muscle (Bulking) you should consume between: " << carbsGainLow << " and " << carbsGainHigh << " grams of carbs. \n";
+        std::cout << "To gain muscle (Bulking) you should consume between: " << fatsGainLow << " and " << fatsGainHigh << " grams of fats. \n\n";
+
+        std::cout << "To achive fat loss and maintain (Lean Out) you should consume between: " << protienFatLossLow << " and " << protienFatLossHigh << " grams of protien. \n";
+        std::cout << "To achive fat loss and maintain (Lean Out) you should consume between: " << carbsFatLossLow << " and " << carbsFatLossHigh << " grams of carbs. \n";
+        std::cout << "To achive fat loss and maintain (Lean Out) you should consume between: " << fatsFatLossLow << " and " << fatsFatLossHigh << " grams of fats. \n\n";
+
+        std::cout << "To maintain muscle you should consume between: " << protienMaintenenceLow << " and " << protienMaintenenceHigh << " grams of protien. \n";
+        std::cout << "To maintain muscle you should consume between: " << carbsMaintenenceLow << " and " << carbsMaintenenceHigh << " grams of carbs. \n";
+        std::cout << "To maintain muscle you should consume between: " << fatsMaintenenceLow << " and " << fatsMaintenenceHigh << " grams of fats. \n";
     }
 
     system("pause");
